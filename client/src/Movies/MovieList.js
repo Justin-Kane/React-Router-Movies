4caaf4595d4bc7d4af128af0a89ac76234d9360e
Justin Kane <JustinKane197@gmail.com>
2019-09-18 11:58:59 -0400

    movie list added

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -27,25 +29,32 @@ const MovieList = props => {
   );
 }
 
-function MovieDetails({ movie }) {
+function MovieDetails({movie}) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+    <Link to={`/movies/${movie.id}`}>
+      <div className="movie-card">
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+        <h2>{title}</h2>
+
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </div>
+
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+
+      </div>
+      
+    </Link>
   );
 }
 
